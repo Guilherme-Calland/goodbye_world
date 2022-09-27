88e0e973e966ea922c65ad2d2630cd8238baddc6
Guilherme-Calland <gui.crb0@gmail.com>
2022-09-27 09:52:44 -0300

    Initial Commit

---
+++ goodbye_world/goodbye_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  goodbye_worldApp.swift
+//  goodbye_world
+//
+//  Created by Guilherme Calland Rosa Borba on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct goodbye_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
