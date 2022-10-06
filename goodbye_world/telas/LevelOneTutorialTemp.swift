2699c7197d6d4fc89e8c4d6950bf19f982ee7f53
Guilherme-Calland <gui.crb0@gmail.com>
2022-10-06 16:52:47 -0300

    animation changes

--- goodbye_world/telas/LevelOneTutorialTemp.swift
+++ goodbye_world/telas/LevelOneTutorialTemp.swift
@@ -11,17 +11,31 @@ struct LevelOneTutorialTemp: View {
     @Environment(\.dismiss) private var dismiss
     
     var panelWidth = UIScreen.main.bounds.width/2.2
-    @State private var fadeIn = true
+    @State private var fadeToWhite = false
+    @State private var imageAppear = false
+    @State private var clickOnScreenAppear = false
+    @State private var actionPanelAppear = false
+    @State private var executionPanelAppear = false
+    @State private var executionButtonAppear = false
+    @State private var clickForNext = false
+    @State private var translucentScreenReady = false
+    @State private var translucentScreenShow = false
+    @State private var imageContrastShow = false
     
     var body: some View {
     
-        ZStack{
+        ZStack(alignment: .bottom){
             ZStack(alignment: .bottomTrailing){
                 HStack{
                     Image("LevelOneTutorial1")
                         .resizable()
                         .interpolation(.none)
-                    Spacer()
+                        .opacity(imageAppear ? 1 : 0)
+                        .onAppear(){
+                            withAnimation(Animation.linear(duration: 1).delay(1.5)){
+                                imageAppear.toggle()
+                            }
+                        }
                     VStack(alignment: .trailing, spacing: 10.0){
                         Rectangle().fill(Color(.white)).frame(height: 10.0)
                         ZStack(alignment: .topLeading){
@@ -33,7 +47,7 @@ struct LevelOneTutorialTemp: View {
                                 .padding()
                                 .border(.black, width: 4)
                         }
-                        .opacity(1.0)
+                        .opacity(actionPanelAppear ? 1 : 0)
                         
                         ZStack(alignment: .topLeading){
                             Rectangle()
@@ -42,10 +56,8 @@ struct LevelOneTutorialTemp: View {
                             Textinho.FonteBonita("Execucao")
                                 .padding()
                                 .border(.black, width: 4)
-                            
                         }.frame(width: panelWidth, height: .infinity)
-                        .opacity(1.0)
-                        
+                            .opacity(executionPanelAppear ? 1 : 0)
                         Rectangle()
                             .fill(Color(.white))
                             .frame(height: 14.0)
@@ -55,23 +67,77 @@ struct LevelOneTutorialTemp: View {
                         .fill(Color(.white))
                         .frame(width: 5.0)
                 }.frame(width: UIScreen.main.bounds.width)
+                
+                
                 Image("execution-buttom")
                     .padding(EdgeInsets.init(top: 0.0, leading: 0.0, bottom: 40, trailing: 20))
-                    .opacity(1.0).onTapGesture {
-                        dismiss()
-                    }
+                    .opacity(executionButtonAppear ? 1 : 0)
+//                    .onTapGesture {
+//                        dismiss()
+//                    }
+            //
+                
             }
+            
+            Rectangle()
+                .fill(Color(.black))
+                .opacity(translucentScreenShow ? 0.5 : 0)
+            ZStack{
+                Rectangle()
+                    .fill(Color(.white))
+                    .frame(width: 800, height: 40)
+                    .border(.black, width: 4)
+                Textinho.FonteBonita("Clique em qualquer lugar de tela para prosseguir")
+                    
+            }
+            .padding(EdgeInsets.init(top: 0.0, leading: 0.0, bottom: 60.0, trailing: 0.0))
+            .opacity(clickForNext ? 1 : 0)
+            .onAppear(){
+                translucentScreenReady = true
+                showClickForNext()
+            }//
+            
             
             InitScreenPlaceholder().onAppear(){
                 withAnimation(Animation.linear(duration: 1.0)){
-                    fadeIn.toggle()
+                    fadeToWhite.toggle()
                 }
-            }.opacity(fadeIn ? 1: 0)
+            }.opacity(fadeToWhite ? 0: 1)
                 
         }
         .navigationBarTitle("")
         .navigationBarHidden(true)
+        .onTapGesture(){
+            if translucentScreenReady {
+                showTranslucentScreen()
+                hideClickForNext()
+            }
+        }
         
+    }
+    
+    func showTranslucentScreen(){
+        withAnimation(Animation.linear(duration: 1.0)){
+            translucentScreenShow = true
+        }
+    }
+    
+    func showClickForNext(){
+        withAnimation(Animation.linear(duration: 1.0).delay(3.0)){
+            clickForNext = true
+        }
+    }
+    
+    func hideClickForNext(){
+        withAnimation(Animation.linear(duration: 1.0)){
+            clickForNext = false
+        }
+    }
+    
+    func toggleContrastImage(){
+        withAnimation(Animation.linear(duration: 1.0)){
+            imageContrastShow.toggle()
+        }
     }
 }
 
